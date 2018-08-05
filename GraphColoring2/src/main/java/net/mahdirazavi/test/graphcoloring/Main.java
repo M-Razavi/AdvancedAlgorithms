383667e8f183643f2797fe12ddb2343748f3e2b6
mahdi <razavi.dev@gmail.com>
2018-08-05 23:32:49 +0430

    1- Fix some bug 2- Add FatJar for gradle

--- GraphColoring2/src/main/java/net/mahdirazavi/test/graphcoloring/Main.java
+++ GraphColoring2/src/main/java/net/mahdirazavi/test/graphcoloring/Main.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.zip.DataFormatException;
@@ -38,13 +37,16 @@ public class Main {
             {1, 1, 0, 1},
             {1, 1, 1, 0}};
 
-//                        int[][] adjacency_matrix=np.array(g1_adj);
-//                        G=nx.from_numpy_array(adjacency_matrix) // Creating the graph given the adjacency matrix;
 
-    //---------------- function generate_r&om_color ----------------;
-// This function receives an integer, & generates that much r&om colors.;
+    /**
+     * This function receives an integer, & generates that much random colors.;
+     *
+     * @param number_of_colors
+     * @return
+     * @throws Exception
+     */
     public static String[] generateColors(int number_of_colors) throws Exception {
-        String[] colorNames = {"Black", "Blue", "Brown", "Orange", "Silver", "Purple", "Red", "Yellow", "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "BlanchedAlmond", "BlueViolet", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "YellowGreen"};
+        String[] colorNames = {"Linen", "Magenta", "Maroon", "YellowGreen", "Black", "Blue", "Brown", "Orange", "Green", "Purple", "Red", "Yellow", "Pink", "Tomato", "Turquoise", "Violet", "LimeGreen", "AliceBlue", "AntiqueWhite", "Aquamarine", "Azure", "Beige", "Bisque", "BlanchedAlmond", "BlueViolet", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "OliveDrab", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Plum", "PowderBlue", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Thistle", "Wheat", "White", "WhiteSmoke"};
         String[] list_of_colors = new String[number_of_colors];
         if (colorNames.length < number_of_colors) {
             throw new Exception("not enough defined color");
@@ -56,10 +58,17 @@ public class Main {
         return list_of_colors;
     }
 
-    //---------------- function assign_color ----------------;
-// This function receives a graph & a list of colors as input, & assigns a valid color in the list to the;
-// nodes of the graph & returns the result graph.;
-    public static Graph assign_color(Graph graph, String[] color) throws Exception {
+
+    /**
+     * This function receives a graph & a list of colors as input, & assigns a valid color in the list to the;
+     * nodes of the graph & returns the result graph.;
+     *
+     * @param graph
+     * @param color
+     * @return
+     * @throws Exception
+     */
+    public static Graph assignColor(Graph graph, String[] color) throws Exception {
 
         for (Iterator it = graph.iterator(); it.hasNext(); ) {
             int node = (int) it.next();
@@ -85,6 +94,42 @@ public class Main {
         return graph;
     }
 
+    /**
+     * Assign a random color for given node.
+     * this assignment maybe not valid you should check with {@link #isColoringValid} method.
+     *
+     * @param graph
+     * @param color
+     * @param node
+     * @return
+     * @throws Exception
+     */
+    public static Graph assignColor(Graph graph, String[] color, int node) {
+        graph.addVertexColor(node, color[new Random().nextInt(color.length)]);
+        return graph;
+    }
+
+    /**
+     * check validity of colors for graph nodes.
+     *
+     * @param graph
+     * @return
+     */
+    public static boolean isColoringValid(Graph graph) {
+        for (Iterator it = graph.iterator(); it.hasNext(); ) {
+            int node = (int) it.next();
+            ArrayList<String> adjacent_colors = new ArrayList<>();
+
+            graph.getVertexNeighbour(node).stream().forEach(n -> adjacent_colors.add(graph.getVertexColor(n)));
+
+            if ((graph.getVertexColor(node).equals("0")) || (adjacent_colors.contains(graph.getVertexColor(node)))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
     /**
      * This function calculates the loss-function for each state;
@@ -102,47 +147,14 @@ public class Main {
 
 
     public static double getProbability(int oldValue, int newValue, int t) {
-        int changed_energy_level = oldValue - newValue;
+        double changed_energy_level = newValue - oldValue;
 
-        if (changed_energy_level >= 0) {
-            return 1;
-        } else ;
-        {
+        if (changed_energy_level <= 0) {
+            return changed_energy_level;
+        } else {
             return Math.E * (changed_energy_level / t);
         }
     }
-
-    /*
-
-
-        //---------------- function choose_next_state ----------------;
-    // This function receives a dictionary of nodes (zzz) & chooses one node r&omly among them all.;
-        public void choose_next_state(graph_, nodes_dict) {
-            (node, color)=r & om.choice(list(nodes_dict.keys()));
-            if (nodes_dict[(node,color)]==1)
-            {
-                graph_.nodes[node]['color'] = color;
-                return graph_;
-                ////// change graph to new state;
-            }
-            else;
-            {
-                ////// generate r&om number an choose an interval, therefore next state;
-                r & = r & om.r & om();
-                if ((r & <= nodes_dict[(node,color)]))
-                {
-                    graph_.nodes[node]['color'] = color;
-                    return graph_;
-                }
-            else;
-                {
-                    return graph_;
-                }
-            }
-        }
-*/
-    //---------------- function simulated_annealing ----------------;
-
 
     /**
      * This function receives a graph, a node of that graph, a color & current temperature;
@@ -163,39 +175,65 @@ public class Main {
     }
 
 
-    public static Graph simulated_annealing(Graph graph, String[] colors, int t) {
+    public static Graph simulatedAnnealing(Graph inputGraph, String[] colors, int t) {
 
-        System.out.println("simulated_annealing-t=" + t);
+        Graph tmpGraph, graph;
+        System.out.println("SA t=" + t + " ColorNumber="+ lossFunction(inputGraph));
         if (t == 0) {
-            return graph;
+            return inputGraph;
         }
+        Graph initialColoredGraph = (Graph) inputGraph.copy();
+        graph = (Graph) inputGraph.copy();
+
+//        System.out.println("isvalid(copy)="+isColoringValid(initialColoredGraph));
         for (Iterator it = graph.iterator(); it.hasNext(); ) {
             int node = (int) it.next();
-            Graph initialColoredGraph = (Graph) graph.copy();
             int oldValue = lossFunction(graph);
 
-            try {
-                graph = assign_color(graph, colors);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                graph = initialColoredGraph;
-            }
-            int newValue = lossFunction(graph);
-            System.out.println("loss=" + newValue);
+            //new neighborhood
+            for (int i = 0; i < 20; i++) {
+                tmpGraph = assignColor(graph, colors, node);
+                if (!isColoringValid(tmpGraph)) {
+                    graph = (Graph) initialColoredGraph.copy();
+                    continue;
+                }
+                graph = tmpGraph;
+                int newValue = lossFunction(graph);
 
-            double probability = getProbability(oldValue, newValue, t);
-            System.out.println("p=" + probability);
-            if (probability <= 0) {
-                graph = initialColoredGraph;
+                double probability = getProbability(oldValue, newValue, t);
+
+                if (probability <= 0) {
+                    //Accept better result
+//                    System.out.println("Accept better result.");
+                    initialColoredGraph = (Graph) graph.copy();
+                    oldValue = newValue;
+                } else if (probability < 0.0500) {
+                    // Accept with if Delta_E < 0.13820
+                    initialColoredGraph = (Graph) graph.copy();
+                    oldValue = newValue;
+//                    System.out.println("accept result with probability=" + probability);
+                }
+                else
+                {
+                    graph = (Graph) initialColoredGraph.copy();
+                }
+
             }
         }
 
         t -= 1;
-        graph = simulated_annealing(graph, colors, t);
+        graph = simulatedAnnealing(initialColoredGraph, colors, t);
 
         return graph;
     }
 
+    /**
+     * Fill graph with adj matrix
+     *
+     * @param adjMatrix
+     * @param graph
+     * @return
+     */
     public static Graph fillGraph(int[][] adjMatrix, Graph graph) {
         ArrayList<Integer> nodes = new ArrayList<>();
         for (int i = 0; i < adjMatrix.length; i++) {
@@ -212,7 +250,7 @@ public class Main {
     }
 
     public static Graph readGraphFromTxt(String filePath) throws IOException, DataFormatException {
-        Graph<Integer> graph = new Graph();
+        Graph graph = new Graph();
 
         File readFile = new File(filePath);
         try (BufferedReader reader = new BufferedReader(new FileReader(readFile.getCanonicalPath()))) {
@@ -235,52 +273,12 @@ public class Main {
                 }
             }
         }
-//        graph.fixObjectInMap();
-
         return graph;
     }
 
-    public static void main(String[] args) throws Exception {
-//        Path path =
-//        String filePath = path.resolve("\\Developments\\AdvancedAlgorithms\\GraphColoring\\Test-Files\\myciel3.col.txt").toAbsolutePath().toString();
-        String filePath = Paths.get("D:\\Developments\\AdvancedAlgorithms\\GraphColoring\\Test-Files\\myciel4.col.txt").toAbsolutePath().toString();
-
-        Graph<Integer> graph = readGraphFromTxt(filePath);
-//        fillGraph(g1_adj, graph);
-        System.out.println(graph);
-
-        int maxDegree = graph.getMaxDegree();
-        String[] colorList = generateColors(maxDegree + 1);
-        System.out.println("Generated Colors:" + Arrays.toString(colorList));
-
-        while (true) {
-            try {
-                graph = assign_color(graph, colorList);
-                break;
-            } catch (Exception e) {
-                //NOP
-            }
-        }
-        System.out.println("First assigned Color:");
-        for (Iterator it = graph.iterator(); it.hasNext(); ) {
-            int node = (int) it.next();
-            System.out.println(node + " : " + graph.getVertexColor(node));
-        }
-
-        int temperature = 5;
-        graph = simulated_annealing(graph, colorList, temperature);
-
-        System.out.println("loss=" + lossFunction(graph));
-        System.out.println("Final assigned Color:");
-        for (Iterator it = graph.iterator(); it.hasNext(); ) {
-            int node = (int) it.next();
-            System.out.println(node + " : " + graph.getVertexColor(node));
-        }
-        ShowGraph(graph);
-    }
-
-    private static void ShowGraph(Graph<Integer> graph) throws InterruptedException {
+    private static void ShowGraph(Graph graph) throws InterruptedException {
         Viewer viewer;
+        System.out.println("Visualize graph....");
         SingleGraph visualGraph = new SingleGraph("Graph Coloring Problem");
         for (Iterator it = graph.iterator(); it.hasNext(); ) {
             int node = (int) it.next();
@@ -288,7 +286,6 @@ public class Main {
             visualGraph.getNode(node + "").addAttribute("ui.style", "fill-color: " + graph.getVertexColor(node) + ";");
 
         }
-//        visualGraph.setStrict(false);
         int c = 0;
         for (Iterator it = graph.iterator(); it.hasNext(); ) {
             int node = (int) it.next();
@@ -297,8 +294,7 @@ public class Main {
                 if (visualGraph.getNode(v + "").hasEdgeBetween(node + "")) {
                     continue;
                 }
-                visualGraph.addEdge((c++) + "", node+"", v+"");
-                Thread.sleep(100);
+                visualGraph.addEdge((c++) + "", node + "", v + "");
             }
         }
 
@@ -309,5 +305,70 @@ public class Main {
         viewer = visualGraph.display(true);
     }
 
+
+    public static void testGraphColor(String[] colors) {
+        Viewer viewer;
+        System.out.println("Visualize graph....");
+        SingleGraph visualGraph = new SingleGraph("Graph Coloring Problem");
+        for (String name : colors) {
+            visualGraph.addNode(name);
+            visualGraph.getNode(name + "").addAttribute("ui.style", "fill-color: " + name + ";");
+
+        }
+
+        for (Node gNode : visualGraph) {
+            gNode.addAttribute("ui.label", gNode.getId());
+        }
+        visualGraph.addAttribute("ui.stylesheet", "node {size: 25px, 25px; shape: box; text-size:17px;}");
+        viewer = visualGraph.display(true);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+//        String[] myColors = generateColors(138);
+//        testGraphColor(myColors);
+
+        String filePath;
+        if (args.length > 0) {
+            filePath = args[0];
+        } else {
+            filePath = Paths.get("D:\\Developments\\AdvancedAlgorithms\\GraphColoring2\\Test-Files\\myciel4.col.txt").toAbsolutePath().toString();
+        }
+        Graph graph = readGraphFromTxt(filePath);
+//        fillGraph(g1_adj, graph);
+        System.out.println("Load graph from : " + filePath);
+        System.out.println(graph);
+
+        int maxDegree = graph.getMaxDegree();
+        String[] colorList = generateColors(maxDegree + 1);
+        System.out.println(colorList.length + " color used for graph at max.");
+
+        while (true) {
+            try {
+                graph = assignColor(graph, colorList);
+                break;
+            } catch (Exception e) {
+                //NOP
+            }
+        }
+//        System.out.println("First assigned Color:");
+//        for (Iterator it = graph.iterator(); it.hasNext(); ) {
+//            int node = (int) it.next();
+//            System.out.println(node + " : " + graph.getVertexColor(node));
+//        }
+
+        int temperature = 100;
+        graph = simulatedAnnealing(graph, colorList, temperature);
+
+        System.out.println(isColoringValid(graph));
+        System.out.println("loss=" + lossFunction(graph));
+        System.out.println("Final assigned Color:");
+        for (Iterator it = graph.iterator(); it.hasNext(); ) {
+            int node = (int) it.next();
+            System.out.print(node + " : " + graph.getVertexColor(node) + " ");
+        }
+        ShowGraph(graph);
+    }
 
 }
